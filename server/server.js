c44ae5ee75463460026f463ae2cdaa6a3fa84f1a
Rahm <rahmatullah.nikyar@gmail.com>
2018-09-09 00:02:02 -0400

    added tumblr consumerkey and secret

--- server/server.js
+++ server/server.js
@@ -3,8 +3,10 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 
-const app = express();
+const tumblr_consumerkey = 'fZD5HJ2LsWPJyHrLChVbv7ViqLtj3kJP41A45HkuD3LA6uyjKW';
+const tumblr_secret = '5kQxkI0E62iUVMYrLfBpMadXdvBGzKgIlKuHYRWSyyI5D3CvWI';
 
+const app = express();
 
 const uri = 'mongodb://codemasher:mash123@ds123171.mlab.com:23171/codemash';
 mongoose.connect(uri, {useNewUrlParser: true});
@@ -13,3 +15,5 @@ mongoose.connect(uri, {useNewUrlParser: true});
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => console.log(`API is running on port ${PORT}`));
+
+console.log("Hello");
